3dc3242eb899db5d9a7e63a0f1d6e572fa2b871b
Dasun Chathuranga <chathurandi35@gmail.com>
2015-03-27 17:36:46 +0530

    Contact form update

--- contact-2/contact.php
+++ contact-2/contact.php
@@ -1,28 +1,26 @@
-<html>
- 
--<head></head>
--<body>
--<?php
--// define variables and set to empty values
--$name = $email = $subject = $message = "";
--
--if ($_SERVER["REQUEST_METHOD"] == "POST") {
--  $name = test_input($_POST["your-name"]);
--  $email = test_input($_POST["your-email"]);
--  $subject = test_input($_POST["your-subject"]);
--  $message = test_input($_POST["your-message"]);
--}
--
--function test_input($data) {
--  $data = trim($data);
--  $data = stripslashes($data);
--  $data = htmlspecialchars($data);
--  return $data;
--}
--
--echo $name;
--?>
--
--</body>
--</html>
+<?php
+$connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server..
+$db = mysql_select_db("flowtech", $connection); // Selecting Database
+//Fetching Values from URL
 
+if(isset($_POST['name'])){
+	$name=$_POST['name'];
+}
+
+if(isset($_POST['email'])){
+	$name=$_POST['email'];
+}
+
+if(isset($_POST['subject'])){
+	$name=$_POST['subject'];
+}
+
+if(isset($_POST['message'])){
+	$name=$_POST['message'];
+}
+
+//Insert query
+$query = mysql_query("insert into contact_table(Name, Email, Subject, Message) values ('$name', '$email', '$subject','$message')");
+echo "Form Submitted Succesfully";
+mysql_close($connection); // Connection Closed
+?>
