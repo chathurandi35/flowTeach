5022e05f0e095577c3fca7e1bcaa193b70068448
Dasun Chathuranga <chathurandi35@gmail.com>
2015-03-27 14:08:40 +0530

    php file update

--- contact-2/contact.php
+++ contact-2/contact.php
@@ -1,1 +1,28 @@
+<html>
+ 
+-<head></head>
+-<body>
+-<?php
+-// define variables and set to empty values
+-$name = $email = $subject = $message = "";
+-
+-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+-  $name = test_input($_POST["your-name"]);
+-  $email = test_input($_POST["your-email"]);
+-  $subject = test_input($_POST["your-subject"]);
+-  $message = test_input($_POST["your-message"]);
+-}
+-
+-function test_input($data) {
+-  $data = trim($data);
+-  $data = stripslashes($data);
+-  $data = htmlspecialchars($data);
+-  return $data;
+-}
+-
+-echo $name;
+-?>
+-
+-</body>
+-</html>
 
